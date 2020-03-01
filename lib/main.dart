9451b498e81b5469683088a0788b978b9227d7f7
nevruzoglu <nevruzoglu@gmail.com>
2020-03-01 23:33:06 +0300

    second

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,32 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PokeLibrary',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text('PokeLibrary'),
+          backgroundColor: Colors.cyan,
         ),
         body: Center(
           child: Container(
             child: Text('Hello World'),
           ),
         ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: null,
+            backgroundColor: Colors.cyan,
+            child: Icon(Icons.refresh)),
+        drawer: Drawer(),
       ),
     );
   }
